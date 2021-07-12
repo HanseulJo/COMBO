9af425cff53b5747337b6df28b9b27bc2fea0f5a
조한슬 <jhs4015@kaist.ac.kr>
2021-07-12 09:46:49 +0900

    More experiments on local_search + start_from_bottom

--- average_graphs.py
+++ average_graphs.py
@@ -5,11 +5,12 @@ import matplotlib.pyplot as plt
 from main_NKmodel import *
 
 #directories = []
-DIRECTORY = '.'  # 'Run###'
+DIRECTORY = '.'  # 'Experiments/Run###'
 IM_SEED = ''  # ''
 
 filenames = glob.glob(os.path.join(f'{DIRECTORY}/NK_COMBO_{IM_SEED}*', 'bo_data.pt'))
 bo_datas = [torch.load(fn) for fn in filenames]
+print(f"Using {len(filenames)} runs to make a plot...")
 
 keys = ['eval_outputs', 'local_optima', 'random_cummax']
 for i in range(len(bo_datas))[::-1]:
@@ -29,13 +30,14 @@ x = torch.arange(1,length+1)
 random_means = random_gap.mean(axis=0)
 random_stds = random_gap.std(axis=0)
 
-plt.plot(x, data_means, linewidth=2, label='COMBO')
+plt.plot(x, data_means, linewidth=2, label='COMBO (local search)')
 plt.fill_between(x, (data_means - data_stds), (data_means + data_stds), color='tab:blue', alpha=0.2)
-plt.plot(x, random_means, linewidth=2, label='Random Search', color='tab:gray')
+plt.plot(x, random_means, linewidth=2, label='Random Walk', color='tab:gray')
 plt.fill_between(x, (random_means - random_stds), (random_means + random_stds), color='tab:gray', alpha=0.1)
 plt.title('Gap: Global Optimum - Tentative Optima (Mean $\pm$ Std.dev)')
 plt.legend()
 plt.xticks(range(0, 21, 4))
+plt.ylim(-0.05, 0.55)
 plt.show()
 
 
--- run_multiple_times.py
+++ run_multiple_times.py
@@ -1,8 +1,8 @@
 import os
 
-command = lambda x: f"python ./main_NKmodel.py --N 6 --K 1 --A 2 --n_eval 20 --interdependency_seed {x[0]} --payoff_seed {x[1]}"
+command = lambda x: f"python ./main_NKmodel.py --N 6 --K 1 --A 2 --n_eval 20 --interdependency_seed {x[0]} --payoff_seed {x[1]} "
 
 for i in range(10):
     for j in range(10):
-        os.system(command([i,j])) # + " --start_from_bottom" + "--local_search"
+        os.system(command([i,j]) + " --local_search") # + " --start_from_bottom" + " --local_search"
 
